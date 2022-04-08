127569bf40c229541ce4c43bdbbd31fe2553a1b4
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2022-04-07 21:22:16 -0300

    Update: ProductOrderResponse.java

--- src/main/java/com/store/api/dto/ProductOrderResponse.java
+++ src/main/java/com/store/api/dto/ProductOrderResponse.java
@@ -1,24 +1,29 @@
 package com.store.api.dto;
 
-import com.store.api.domain.Product;
+import com.store.api.domain.Customer;
 import com.store.api.domain.ProductOrders;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Getter @Setter
 public class ProductOrderResponse {
     private UUID orderCode;
+    private UUID customerCode;
     private ZonedDateTime orderTime;
     private List<ProductResponse> products;
 
     public static ProductOrderResponse converter(ProductOrders productOrders) {
+        Optional<Customer> customer = productOrders.getCustomers().stream().findFirst();
+
         ProductOrderResponse productOrderResponse = new ProductOrderResponse();
         productOrderResponse.setOrderCode(productOrders.getOrderCode());
+        productOrderResponse.setCustomerCode(customer.get().getCustomerCode());
         productOrderResponse.setOrderTime(productOrders.getOrderTime());
         productOrderResponse.setProducts(productOrders.getProducts().stream().map(ProductResponse::converter).collect(Collectors.toList()));
 
