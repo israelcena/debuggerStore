eab49d0c0954021ded71e9110ae85dfb2a6daf95
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2022-04-07 22:19:45 -0300

    Update: ProductOrderResponse.java

--- src/main/java/com/store/api/dto/ProductOrderResponse.java
+++ src/main/java/com/store/api/dto/ProductOrderResponse.java
@@ -1,10 +1,12 @@
 package com.store.api.dto;
 
 import com.store.api.domain.Customer;
+import com.store.api.domain.Product;
 import com.store.api.domain.ProductOrders;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -17,15 +19,17 @@ public class ProductOrderResponse {
     private UUID customerCode;
     private ZonedDateTime orderTime;
     private List<ProductResponse> products;
+    private BigDecimal priceTotal;
 
     public static ProductOrderResponse converter(ProductOrders productOrders) {
         Optional<Customer> customer = productOrders.getCustomers().stream().findFirst();
+        List<Product> products = productOrders.getProducts();
 
         ProductOrderResponse productOrderResponse = new ProductOrderResponse();
         productOrderResponse.setOrderCode(productOrders.getOrderCode());
         productOrderResponse.setCustomerCode(customer.get().getCustomerCode());
         productOrderResponse.setOrderTime(productOrders.getOrderTime());
-        productOrderResponse.setProducts(productOrders.getProducts().stream().map(ProductResponse::converter).collect(Collectors.toList()));
+        productOrderResponse.setProducts(products.stream().map(ProductResponse::converter).collect(Collectors.toList()));
 
         return productOrderResponse;
     }
